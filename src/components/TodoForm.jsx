560a8ea075a343150608ef564d9b06f2454ce1c3
MURALITHARAN D <131011398+dmmuralitharan@users.noreply.github.com>
2025-01-12 20:04:14 +0530

    update formik to todo add form

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,46 +1,43 @@
 import PropTypes from "prop-types";
-import { useState } from "react"
+// import { useState } from "react"
 import { useTodos } from "../hooks/useTodos";
+import { useFormik } from 'formik'
 
 function TodoForm() {
 
   const { handleAddTodo } = useTodos()
 
-  const [todoFormData, setTodoFormData] = useState({
-    title: ''
-  })
+  const formikTodoForm = useFormik({
+    initialValues: {
+      title: '',
+    },
+    validate: values => {
+      const errors = {};
+      if (!values.title) {
+        errors.title = 'Required';
+      } else if (values.title.length > 20) {
+        errors.title = 'Must be 20 characters or less';
+      }
 
-  function handleTodoInputChange(event) {
-
-    const { value } = event.target
-
-    setTodoFormData({
-      ...todoFormData,
-      title: value
-    })
-
-  }
-
-
-  function handleTodoFormSubmit(event) {
-
-    event.preventDefault()
-
-    console.log(todoFormData);
-
-    if (todoFormData.title.trim() !== "") {
-      handleAddTodo({ title: todoFormData.title });
-      setTodoFormData({ title: "" });
+      return errors
+    },
+    onSubmit: values => {
+      handleAddTodo({ title: values.title });
+      values.title = ''
     }
-
-  }
+  })
 
   return (
     <>
 
-      <form className="mx-3 mt-10 w-full flex flex-col gap-y-5" onSubmit={handleTodoFormSubmit}>
-        <input type="text" id="title" name="title" placeholder="Hello world" className="flex-1 px-2.5 py-2 rounded-md" value={todoFormData.title} onChange={handleTodoInputChange} />
-        <button type="submit" className="bg-slate-900 text-slate-100 py-2.5 rounded-md font-semibold">Add Todo</button>
+      <form className="mx-3 mt-10 w-full flex flex-col" onSubmit={formikTodoForm.handleSubmit}>
+        <input type="text" id="title" name="title" placeholder="Hello world" className="flex-1 px-2.5 py-2 rounded-md"
+          value={formikTodoForm.values.title}
+          onChange={formikTodoForm.handleChange}
+          onBlur={formikTodoForm.handleBlur}
+        />
+        {formikTodoForm.errors.title && <div className="text-red-500 ms-2 mt-1 text-sm">{formikTodoForm.errors.title}</div>}
+        <button type="submit" className="bg-slate-900 text-slate-100 mt-2.5 py-2.5 rounded-md font-semibold">Add Todo</button>
       </form>
 
     </>
